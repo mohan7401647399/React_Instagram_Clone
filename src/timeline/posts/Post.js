3b6b6db0e2d28bdb86e734ebdff8ef5b43e28dc3
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-07-23 07:46:59 +0530

    Update Post.js

--- src/timeline/posts/Post.js
+++ src/timeline/posts/Post.js
@@ -7,29 +7,29 @@ import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 
-function Post({user,postImage,like,timeStamp}) {
+function Post({ user, postImage, like, timeStamp }) {
   return (
     <div className='post'>
       <div className='post_header'>
         <div className='post_headerauthor'>
-            <Avatar>{user.charAt(0).toUpperCase()}</Avatar>
-           <div className='divuser'>{user}</div> . <span>{timeStamp}</span>
+          <Avatar>{user.charAt(0).toUpperCase()}</Avatar>
+          <div className='divuser'>{user}</div> . <span>{timeStamp}</span>
         </div>
-        <MoreHorizIcon/>
+        <MoreHorizIcon />
       </div>
       <div className='post_image'>
-        <img src={postImage}/>
+        <img src={postImage} alt='postImage' />
       </div>
       <div className='post_footer'>
         <div className='post_footericons'>
-            <div className='post_iconmain'>
-                < FavoriteBorderIcon className='postIcon'/>
-                < ChatBubbleOutlineIcon className='postIcon'/>
-                < TelegramIcon className='postIcon'/>
-            </div>
-            <div className='post_inconsave'>
-                <BookmarkBorderIcon className='postIcon'/>
-            </div>
+          <div className='post_iconmain'>
+            < FavoriteBorderIcon className='postIcon' />
+            < ChatBubbleOutlineIcon className='postIcon' />
+            < TelegramIcon className='postIcon' />
+          </div>
+          <div className='post_inconsave'>
+            <BookmarkBorderIcon className='postIcon' />
+          </div>
         </div>
         Liked by {like} people
       </div>
