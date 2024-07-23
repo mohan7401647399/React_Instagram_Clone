900fab0e292901a6f2151a6c81150200e7b60ad9
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-07-23 07:45:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,34 @@ import Authentication from './Authentication/Authentication'
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { auth } from './firebase';
-import {loginUser, setLoading} from './features/userSlice'
+import { loginUser, setLoading } from './features/userSlice'
 
 function App() {
   const dispatch = useDispatch()
-  
+
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      if(authUser){
+      if (authUser) {
         dispatch(loginUser({
           uid: authUser.uid,
           username: authUser.displayName,
           email: authUser.email,
         }));
         dispatch(setLoading(false));
-      }else{
+      } else {
         dispatch(setLoading(false));
         console.log("User is not logged in");
       }
     })
-  },[]);
+    // eslint-disable-next-line
+  }, []);
   const user = useSelector((state) => state.data.user.user);
-  const isLoading = useSelector((state)=> state.data.user.isLoading)
+  const isLoading = useSelector((state) => state.data.user.isLoading)
   return (
-  <div className="app">
-    {isLoading ? (<div class="loader"><div class="spinner"></div></div>)
-     : ( <>{user ? <Homepage/> : <Authentication/>}</>)}
-  </div>
-  )};
+    <div className="app">
+      {isLoading ? (<div class="loader"><div class="spinner"></div></div>)
+        : (<>{user ? <Homepage /> : <Authentication />}</>)}
+    </div>
+  )
+};
 export default App;
