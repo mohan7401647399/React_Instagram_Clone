68b1ead7694cf78ff8b4f3786c0489ad00756f9c
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-07-23 07:46:08 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD9noKS-XvLmcd-itRoxkWN2ThDBh-RB4E",
@@ -10,5 +10,6 @@ const firebaseConfig = {
   appId: "1:685322843788:web:473f98fc503903877feda9"
 };
 
+// eslint-disable-next-line
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
